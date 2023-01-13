c491bcc5114f31771d91125873fbb4e3e0667876
CaptainGinny <ginnymalko@gmail.com>
2023-01-13 09:29:47 +0300

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles/reset.css";
 import "./styles/main.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./Components/navbar/Navbar";
 import Footer from "./Components/footer/Footer";
