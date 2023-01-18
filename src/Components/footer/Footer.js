838eb469ca4b12271a3ae6df0e4fc4322b78cb3f
CaptainGinny <ginnymalko@gmail.com>
2023-01-18 12:45:21 +0300

    added social media links

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -11,17 +11,17 @@ export default function Footer() {
           <div className="footer__wrapper">
             <ul className="social">
               <li className="social__item">
-                <a href="#!">
+                <a href="https://twitter.com/Captain__Ginny">
                   <img src={twitter} alt="Link" />
                 </a>
               </li>
               <li className="social__item">
-                <a href="#!">
+                <a href="https://github.com/CaptainGinny">
                   <img src={gitHub} alt="Link" />
                 </a>
               </li>
               <li className="social__item">
-                <a href="#!">
+                <a href="https://www.linkedin.com/in/yauheni-malko-54a031257">
                   <img src={linkedIn} alt="Link" />
                 </a>
               </li>
