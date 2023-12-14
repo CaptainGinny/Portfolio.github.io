095e1e2ab8be7d4eb02b0d98c5df9457cbb479ce
CaptainGinny <ginnymalko@gmail.com>
2023-12-14 17:41:31 +0300

    Added a new project  pizza

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,84 +1,92 @@
-import watchproject from "./../img/projects/watch.png";
-import watchprojectBig from "./../img/projects/watch-big.png";
+import watchproject from './../img/projects/watch.png';
+import watchprojectBig from './../img/projects/watch-big.png';
 
-import mycompanyproject from "./../img/projects/mycompany.png";
-import mycompanyprojectBig from "./../img/projects/mycompany-big.png";
+import mycompanyproject from './../img/projects/mycompany.png';
+import mycompanyprojectBig from './../img/projects/mycompany-big.png';
 
-import thirdproject from "./../img/projects/sitelayout.png";
-import thirdprojectBig from "./../img/projects/sitelayout-big.png";
+import thirdproject from './../img/projects/sitelayout.png';
+import thirdprojectBig from './../img/projects/sitelayout-big.png';
 
-import reactpractice from "./../img/projects/reactpractice.png"; 
-import reactpracticeBig from "./../img/projects/reactpractice-big.png";
+import reactpractice from './../img/projects/reactpractice.png';
+import reactpracticeBig from './../img/projects/reactpractice-big.png';
 
-import project05 from "./../img/projects/relvise.png";
-import project05Big from "./../img/projects/relvise-big.png";
+import project05 from './../img/projects/relvise.png';
+import project05Big from './../img/projects/relvise-big.png';
 
-import colours from "./../img/projects/colours.png";
-import coloursBig from "./../img/projects/colours-big.png";
+import colours from './../img/projects/colours.png';
+import coloursBig from './../img/projects/colours-big.png';
 
-import calculator from "./../img/projects/calculator.png";
-import calculatorBig from "./../img/projects/calculator-big.png";
+import calculator from './../img/projects/calculator.png';
+import calculatorBig from './../img/projects/calculator-big.png';
+
+import reactpizza from './../img/projects/react-pizza.png';
+import reactpizzaBig from './../img/projects/react-pizza.png';
 
 const projects = [
   {
     id: 0,
-    title: "The First project-practic",
-    skills: "HTML, CSS",
+    title: 'The First project-practic',
+    skills: 'HTML, CSS',
     img: watchproject,
     imgBig: watchprojectBig,
-    gitHubLink: "https://captainginny.github.io/TheFirstProject-practic",
+    gitHubLink: 'https://captainginny.github.io/TheFirstProject-practic',
   },
   {
     id: 1,
-    title: "Second practical project",
-    skills: "HTML, CSS, JavaScript",
+    title: 'Second practical project',
+    skills: 'HTML, CSS, JavaScript',
     img: mycompanyproject,
     imgBig: mycompanyprojectBig,
-    gitHubLink:
-      "https://captainginny.github.io/Second-practical-project.github.io",
+    gitHubLink: 'https://captainginny.github.io/Second-practical-project.github.io',
   },
   {
     id: 2,
-    title: "Third-project-sitelayout",
-    skills: "HTML, CSS, JavaScript, Owl Carousel",
+    title: 'Third-project-sitelayout',
+    skills: 'HTML, CSS, JavaScript, Owl Carousel',
     img: thirdproject,
     imgBig: thirdprojectBig,
-    gitHubLink:
-      "https://captainginny.github.io/Third-project-sitelayout.github.io",
+    gitHubLink: 'https://captainginny.github.io/Third-project-sitelayout.github.io',
   },
   {
     id: 3,
-    title: "React-Typescript-practice",
-    skills: "React, Typescript, JavaScript, Node.js",
+    title: 'React-Typescript-practice',
+    skills: 'React, Typescript, JavaScript, Node.js',
     img: reactpractice,
     imgBig: reactpracticeBig,
-    gitHubLink:
-      "https://captainginny.github.io/React-and-typescript-practice.github.io/",
+    gitHubLink: 'https://captainginny.github.io/React-and-typescript-practice.github.io/',
   },
   {
     id: 4,
-    title: "Project-layout-training",
-    skills: "HTML, CSS, Responsive Web Design",
+    title: 'Project-layout-training',
+    skills: 'HTML, CSS, Responsive Web Design',
     img: project05,
     imgBig: project05Big,
-    gitHubLink:
-      "https://captainginny.github.io/Project-layout-training.github.io",
+    gitHubLink: 'https://captainginny.github.io/Project-layout-training.github.io',
   },
   {
     id: 5,
-    title: "Project-Colours",
-    skills: "HTML, CSS, JavaScript",
+    title: 'Project-Colours',
+    skills: 'HTML, CSS, JavaScript',
     img: colours,
     imgBig: coloursBig,
-    gitHubLink: "https://captainginny.github.io/Project-Coluors-github.io",
+    gitHubLink: 'https://captainginny.github.io/Project-Coluors-github.io',
   },
   {
     id: 6,
-    title: "Loan-Calculator",
-    skills: "JavaScript, HTML",
+    title: 'Loan-Calculator',
+    skills: 'JavaScript, HTML',
     img: calculator,
     imgBig: calculatorBig,
-    gitHubLink: "https://captainginny.github.io/Loan-Calculator/",
+    gitHubLink: 'https://captainginny.github.io/Loan-Calculator/',
+  },
+  {
+    id: 7,
+    title: 'React-Pizza',
+    skills:
+      'ReactJS 18, TypeScript, Redux Toolkit, React Router v6, Axios + Fetch, React Hooks, Prettier, CSS-Modules / SCSS, React Content Loader, React Pagination, Lodash.Debounce, Code Splitting, React Loadable',
+    img: reactpizza,
+    imgBig: reactpizzaBig,
+    gitHubLink: 'https://captainginny.github.io/react-pizza.github.io/',
   },
 ];
 
